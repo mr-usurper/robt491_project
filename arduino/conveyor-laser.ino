368eff8b4bc36b9b11af1833f52f6f80d2228596
Mazhit <64830548+mr-usurper@users.noreply.github.com>
2023-03-16 22:46:33 +0600

    Update conveyor-laser.ino

--- arduino/conveyor-laser.ino
+++ arduino/conveyor-laser.ino
@@ -1,18 +1,22 @@
 //moving after laser reading
+#include "millisDelay.h"
 
 #define enA 9
 #define in1 6
 #define in2 7
 
 int laser = 2;
-int laser2 = 13;
+int laser2 = 3;
 int bluepin = 11; 
 int greenpin = 12; 
 int redpin = 13; 
 
 int item = 0; 
-int thresh1 = 50;
-int thresh2 = 50;
+int thresh1 = 30;
+int thresh2 = 40;
+int gogo = 0;
+
+millisDelay pinDelay;
 
 void setup() {
   Serial.begin(9600);
@@ -32,42 +36,66 @@ void setup() {
   digitalWrite(laser, HIGH); 
   digitalWrite(laser2, HIGH);
 
+  pinDelay.start(1000);
+
 }
 
 void loop() {
-
   int value1 = map(analogRead(A3), 0, 1023, 100, 0);
   int value2 = map(analogRead(A0), 0, 1023, 100, 0);
+  
+  if (pinDelay.justFinished()) {
+    gogo = 1;
+  } else if (gogo == 1) {
+    int value1 = map(analogRead(A3), 0, 1023, 100, 0);
+    int value2 = map(analogRead(A0), 0, 1023, 100, 0);
+  
+    //Serial.print(value1);
+    //Serial.print(" ");
+    //Serial.println(value2);
+    delay(500);
+  
+    if (item == 0) {
+        if (value1 > thresh1) {
+          digitalWrite(in1, LOW);
+          digitalWrite(in2, HIGH);
+          analogWrite(enA, 0);
+          analogWrite(redpin, 0);
+          analogWrite(greenpin, 255);
+          //Serial.println("no item");
 
-  Serial.println(value2);
-  delay(500);
+          Serial.println("13");
+          
+        } else if (value1 < thresh1) {
+          //Serial.println("SAW ITEM");
+          item = 1;
+        }
+    } else if (item == 1) {
+        if ( value2 > thresh2) {
+          digitalWrite(in1, LOW);
+          digitalWrite(in2, HIGH);
+          analogWrite(enA, 255);
+          analogWrite(redpin, 255);
+          analogWrite(greenpin, 0);
+          //Serial.println("RUN");
 
-  if (item == 0) {
-      if (value1 > thresh1) {
+          Serial.println("69");
+          
+        } else if (value2 < thresh2) {
+          //Serial.println("DONE DEAL");
+          item = 2;
+        }
+    } else if (item == 2) {
         digitalWrite(in1, LOW);
         digitalWrite(in2, HIGH);
         analogWrite(enA, 0);
-        Serial.println("no item");
-      } else if (value1 < thresh1) {
-        Serial.println("SAW ITEM");
-        item = 1;
-      }
-  } else if (item == 1) {
-      if ( value2 > thresh2) {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        analogWrite(enA, 255);
-        Serial.println("RUN");
-      } else if (value2 < thresh2) {
-        Serial.println("DONE DEAL");
-        item = 2;
-      }
-  } else if (item == 2) {
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-      analogWrite(enA, 0);
-      Serial.println("no item");
-      
+        analogWrite(redpin, 0);
+        analogWrite(greenpin, 255);
+        //Serial.println("no item");
+        Serial.println("77");
+        item = 0;
+        
+    }
   }
-
+  
 }
